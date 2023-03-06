d2e1ce8d81619b5579c4d9f490d1ea05c7eca13f
blackhane <why_not_cry@naver.com>
2023-03-06 17:34:38 +0900

    2023-03-06 parkjinhwi

--- beauty/src/main/java/kr/co/beauty/controller/Order2Controller.java
+++ beauty/src/main/java/kr/co/beauty/controller/Order2Controller.java
@@ -14,34 +14,16 @@ import kr.co.beauty.service.Order2Service;
 import kr.co.beauty.vo.OrderVO;
 import kr.co.beauty.vo.OrdercompleteVO;
 import kr.co.beauty.vo.Product2VO;
-import lombok.extern.log4j.Log4j2;
 
-@Log4j2
 @MapperScan("kr.co.beauty.dao")
 @Controller
 public class Order2Controller {
 	
 	@Autowired
 	private Order2Service service;
-	
-	//상세보기 > 주문결제 (비회원)
-	@GetMapping("order/orderform/public")
-	public String order2(Model model, int prodNo, int count, String color, String size) {
-		Product2VO vo = service.selectProduct(prodNo);
-		vo.setCount(count);
-		vo.setColor(color);
-		vo.setSize(size);
-		List<Product2VO> list = new ArrayList<>();
-		list.add(vo);
-		
-		model.addAttribute("list",list);
-		
-		return "order/orderform";
-	}
 	//상세보기 > 주문결제 (회원)
 	@GetMapping("order/orderform")
 	public String order2(Model model, Principal principal, int prodNo, int count, String color, String size) {
-		String user = principal.getName();
 		Product2VO vo = service.selectProduct(prodNo);
 		vo.setCount(count);
 		vo.setColor(color);
