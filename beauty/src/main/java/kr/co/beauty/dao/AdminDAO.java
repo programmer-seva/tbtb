a34c552adefa30f599ed3395725a258683cc28d5
vhfl1 <111475086+vhfl1@users.noreply.github.com>
2023-03-13 13:43:27 +0900

    2023-03-13 admin product list 윤사랑

--- beauty/src/main/java/kr/co/beauty/dao/AdminDAO.java
+++ beauty/src/main/java/kr/co/beauty/dao/AdminDAO.java
@@ -9,9 +9,9 @@ import kr.co.beauty.vo.Product1VO;
 @Repository
 public interface AdminDAO {
 	public int insertProduct(Product1VO vo);
-	public List<Product1VO> selectProducts(String param1);
-	public List<Product1VO> selectProductByCate1(String param1,String arg1);
-	public List<Product1VO> selectProductByCheckBox(List<String> checkBoxArr);
+	public List<Product1VO> selectProducts(String param1,String arg1, int param3);
+	public List<Product1VO> selectProductByCheckBox(List<String> collection);
 	public int deleteProduct(String prodNo);
 	public List<Product1VO> searchProduct(String param1,String arg1,String param2,String arg2);
+	public int selectCountTotal();
 }
