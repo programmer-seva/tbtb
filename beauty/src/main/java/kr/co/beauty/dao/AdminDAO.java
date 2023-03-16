6c63fc7d0ba024afbe7129dd1b2f0fbdc09aec34
dbswhdns <dbswhdns@naver.com>
2023-03-17 02:11:56 +0900

    2023-03-17 admin productlist 윤사랑

--- beauty/src/main/java/kr/co/beauty/dao/AdminDAO.java
+++ beauty/src/main/java/kr/co/beauty/dao/AdminDAO.java
@@ -12,6 +12,6 @@ public interface AdminDAO {
 	public List<Product1VO> selectProducts(List<String> collection);
 	public int selectCountProducts(List<String> collection);
 	public int deleteProduct(String prodNo);
-	public List<Product1VO> searchProduct(String arg1,String param2,String arg2);
+	public List<Product1VO> searchProduct(String[] arg0,String param2,String arg2);
 	public int selectCountTotal();
 }
--- beauty/src/main/resources/static/js/admin.js
+++ beauty/src/main/resources/static/js/admin.js
@@ -22,6 +22,183 @@ $(document).ready(function(){
     	}
     });
     
+    //페이지가 로드되면 처음 실행
+  	CheckBoxList();//상품목록
+  	page();//페이지 번호
+  	
+  	var start=0;
+  	var pg=1;
+  	var totalPage=1;
+  	//체크된 메뉴 상품목록 불러오기
+	function CheckBoxList(){
+		//체크박스 값 배열에 담기
+		var collection = new Array();
+	  	if($('input:checkbox[name=category2]:checked').length == 0){
+	  		//전체 값
+			$('input[name=category2]').each(function(){
+				collection.push($(this).val());
+			});	
+	  	}else{
+	  		//선택된 값
+			$('input[name=category2]:checked').each(function(){
+				collection.push($(this).val());
+			});	
+	  	}
+	  	//console.log(collection);
+		
+		//AJAX 요청 보내기
+	  	$.ajax({
+			type:"post",
+			url:"/Beauty/admin/product/list",
+			data: {'collection':collection},
+			dataType: 'json',
+			success:function(data){
+				//console.log(data);
+				$('.productRow').remove();
+				//최신순 정렬
+				const sortedResult = data.result.sort((a,b)=>b.prodNo-a.prodNo);
+				//상품목록 불러오기
+				let tag = "";
+				//상품목록에 상품 개수 20개까지만 보이게 함
+				const sliceArr = sortedResult.slice(start,start+20);
+				
+				for(let i=0; i<sliceArr.length; i++){
+						tag += "<tr class='productRow'>";
+						tag += "<td><input type='checkbox' name='선택체크' class='rowCheck' value='"+sliceArr[i].prodNo+"'></td>";
+						tag += "<td><img src='#'></td>";
+						tag += "<td>"+sliceArr[i].prodNo+"</td>";
+						tag += "<td>"+sliceArr[i].prodCate1+"</td>";
+						tag += "<td>"+sliceArr[i].prodCate2+"</td>";
+						tag += "<td>"+sliceArr[i].prodName+"</td>";
+						tag += "<td>"+sliceArr[i].disPrice+"</td>";
+						tag += "<td>"+sliceArr[i].stock+"</td>";
+						tag += "<td>"+sliceArr[i].hit+"</td>";
+						tag += "<td>";
+						tag += "<button class='deleteButton' value='"+sliceArr[i].prodNo+"'>삭제</button>";
+						tag += "</td>";
+						tag += "</tr>"
+				}
+				$('#tableList').append(tag);
+			}
+		});
+  	}
+	
+	//페이징 처리
+	function page(){
+		//체크박스 값 배열에 담기
+		var collection = new Array();
+	  	if($('input:checkbox[name=category2]:checked').length == 0){
+	  		//전체 값
+			$('input[name=category2]').each(function(){
+				collection.push($(this).val());
+			});	
+	  	}else{
+	  		//선택된 값
+			$('input[name=category2]:checked').each(function(){
+				collection.push($(this).val());
+			});	
+	  	}
+		//ajax 요청 보내기
+	  	$.ajax({
+			type:"post",
+			url:"/Beauty/admin/product/listCount",
+			data: {'collection':collection},
+			dataType: 'json',
+			success:function(data){
+				//console.log("상품개수",data.result);
+				//전체 페이지 수 정의
+				if(data.result % 20 == 0){
+					totalPage=data.result/20;
+				}else{
+					totalPage=Math.ceil(data.result/20);
+				}
+				//console.log("page??",totalPage);
+				//페이지 개수 정하기
+				UpdatePg(totalPage);
+			}
+	  	});
+	}
+	
+	//현재 페이지 번호 기준으로 앞뒤 5개씩의 페이지 번호만 보여줌
+  	function UpdatePg(totalPage){
+  		
+  		var startPg = Math.max(pg-5,1);
+  		
+  		if(pg<6 && totalPage>=10){
+  			var endPg = 10;
+  		}else if(pg>=6 && totalPage>=10){
+  			var endPg = Math.min(pg+4,totalPage);
+  		}
+  		//console.log("startPg",startPg);
+  		//console.log("endPg",endPg);
+  		//console.log("totalPage",totalPage);
+  		$(".page").empty();
+		//페이지 만들기
+  		let tag = "<span class='prev'>이전</span>";
+		
+  		for(i=startPg; i<=endPg; i++){
+			let num= pg==i?'current':'num';
+			tag += "<span class='"+num+"' data-value='"+i+"'>"+i+"</span>";
+		}
+			tag += "<span class='next'>다음</span>";
+		
+		$(".page").append(tag);
+  	}
+	
+	//페이지 번호 클릭했을 때
+	$(document).on("click",".num", function(){
+		pg = $(this).attr("data-value");
+		start=(pg-1)*20;
+    	$(".current").attr("class","num");
+	    $(this).attr("class","current");
+		
+	    CheckBoxList();
+	    page();
+	});
+	
+	//이전 버튼 클릭했을 때
+	$(document).on("click",".prev", function(){
+		pg--;
+		start=(pg-1)*20;
+		//이전 페이지가 없을 경우
+		if(pg<1){
+			pg=1;
+			alert("첫번째 페이지입니다.");
+			return false;
+		}
+		
+		$(".current").attr("class","num");
+		$(".num").eq(pg-1).attr("class","current");
+		
+		CheckBoxList();
+		page();
+	});
+	
+	//다음 버튼 클릭했을 때
+	$(document).on("click",".next", function(){
+		pg++;
+		start=(pg-1)*20;
+		var maxPg = $(".num").length+1;
+		//console.log(maxPg);
+		//다음 페이지가 없을 경우
+		if(pg>totalPage){
+			alert("마지막 페이지입니다.");
+			pg=totalPage;
+			return false;	
+		}
+		
+		$(".current").attr("class","num");
+		$(".num").eq(pg-1).attr("class","current");
+		CheckBoxList();	
+		page();
+		
+	});
+	
+  	//메뉴 체크박스가 변화할 때마다 상품목록 불러오기
+	$("ul").find("input[type=checkbox]").on("change",function(){
+    	CheckBoxList();
+    	page();
+    });
 	
 
 	/* 상품목록 체크박스 제어 */
@@ -35,7 +212,7 @@ $(document).ready(function(){
 	});
 	
     //상품목록 체크박스 개별선택
-    $(".rowCheck").on("click",function(){
+    $(document).on('click', '.rowCheck', function(){
     	if($(this).prop("checked") == false){
     		$("#checkAll").prop("checked", false);
     	}else if($(".rowCheck:checked").length == $(".rowCheck").length){
@@ -43,30 +220,65 @@ $(document).ready(function(){
     	}
     });
     
-    //체크박스 선택삭제
+    //삭제버튼을 이용한 상품삭제
+	$(document).on('click', '.deleteButton', function(){
+		var prodNo = $(this).val();
+		//console.log(prodNo,"prodNo");
+
+		$.ajax({
+	        url: '/Beauty/admin/product/list/delete',
+	        type: 'get',
+	        data: {prodNo: prodNo},
+	        success: function(data) {
+	            //console.log("data : ", data);
+	            //삭제된 행을 제거
+	            $(this).closest("tr").remove();
+	            CheckBoxList();
+    			page();
+	        }.bind(this)   
+        });
+		
+		
+	});
+    //체크박스를 이용한 상품삭제
 	$(".delete").on("click",function(){
 		
-		var checkBoxArr = new Array();
-		
-		$(".rowCheck:checked").each(function(){
-			checkBoxArr.push($(this).val());
+			var checkBoxArr = new Array();
 			
-		});
-		
-		if(checkBoxArr.length == 0){
-			alert("선택된 글이 없습니다.");
-		}else{
-			$.ajax({
-				url:'/Beauty/admin/product/list/delete',
-				type:'post',
-				data:{'checkBoxArr':checkBoxArr},
-				success:function(data){
-					console.log("data : ", data);
-					location.reload();
-				}	
+			$(".rowCheck:checked").each(function(){
+				checkBoxArr.push($(this).val());
+				
 			});
-		}
+			
+			if(checkBoxArr.length == 0){
+				alert("선택된 글이 없습니다.");
+			}else{
+				$.ajax({
+					url:'/Beauty/admin/product/list/delete',
+					type:'post',
+					data:{'checkBoxArr':checkBoxArr},
+					success:function(data){
+						//console.log("data : ", data);
+						// 삭제된 행을 제거
+		                $(".rowCheck:checked").each(function(){
+		                    $(this).closest("tr").remove();
+		                });
+		                CheckBoxList();
+    					page();
+					}	
+				});
+			}
 	});
+	
+	//상품검색할 때 소분류 카테고리 값 form에 담기
+	$("input[name='category2']").on("change", function() {
+	    var cate2 = $.map($("input[name='category2']:checked"), function(element){
+	      return $(element).val();
+	    });
+	    
+	    $("#cate2").val(cate2);
+	    console.log(cate2);
+	  });
 });
 
 /* product-register 카테고리 분류 */
