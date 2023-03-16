f8b24e4bc3c8538514d3d090e6e5ce692518ec8a
KimDongGeun200418 <chie44911@gmail.com>
2023-03-16 13:50:01 +0900

    2023-03-16 mypage kdg

--- beauty/src/main/resources/static/js/myshop/profile.js
+++ beauty/src/main/resources/static/js/myshop/profile.js
@@ -1,6 +1,23 @@
 $(function(){
 
 	/* 위시리스트 전체삭제 */
-	
+	$('#btnCheckPW').click(function(e){
+		e.preventDefault();
+		let pw = $('#pw').val();
+		$.ajax({
+			url:'/Beauty/myshop/checkPW',
+			type:'POST',
+			data:{'pw': pw},
+			dataType:'json',
+			success:function(data){
+				if(data == 1){
+					
+				}else{
+					alert('비밀번호가 일치하지 않습니다.');
+				}
+			}
+		});
+		
+	});
 
 });
