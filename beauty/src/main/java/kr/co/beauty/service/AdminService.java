3e77bb0b04d5066d98421e1a0f9b197de91494c1
vhfl1 <111475086+vhfl1@users.noreply.github.com>
2023-03-28 11:12:26 +0900

    2023 2

--- beauty/src/main/java/kr/co/beauty/service/AdminService.java
+++ beauty/src/main/java/kr/co/beauty/service/AdminService.java
@@ -10,126 +10,47 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import jakarta.transaction.Transactional;
 import kr.co.beauty.dao.AdminDAO;
 import kr.co.beauty.vo.Product1VO;
 
 @Service
 public class AdminService {
-
+	
 	@Autowired
 	private AdminDAO dao;
-
-	// 상품등록
+	
+	//상품등록
 	public int insertProduct(Product1VO vo) {
-		// 파일 업로드
+		//파일 업로드
 		fileUpload(vo);
-
-		// 할인된 가격 등록
+		
+		//할인된 판매가격 등록
 		int price = vo.getPrice();
 		int discount = vo.getDiscount();
-		int disPrice = price * discount / 100;
+		int disPrice = price * (100-discount)/100;
 		vo.setDisPrice(disPrice);
-
-		// 상품등록
-		return dao.insertProduct(vo);
-	};
-
-	// 상품 옵션 등록
-	public int insertOption(int param1, List<String> colorArr, List<String> colorNameArr, List<String> sizeArr) {
-		int result = 0;
-
-		for (int i = 0; i < colorArr.size(); i++) {
-			String param2 = colorArr.get(i);
-			String param3 = colorNameArr.get(i);
-			for (int j = 0; j < sizeArr.size(); j++) {
-				String param4 = sizeArr.get(j);
-				dao.insertOption(param1, param2, param3, param4);
-			}
-		}
+		
+		//상품등록
+		int result = dao.insertProduct(vo);
 		return result;
-	};
-
-	// 관리자페이지에서 선택된 카테고리의 상품목록 불러오기
-	public List<Product1VO> selectProducts(List<String> collection) {
-		return dao.selectProducts(collection);
-	}
-
-	// 상품 삭제
-	public int deleteProduct(String prodNo) {
-		return dao.deleteProduct(prodNo);
-	}
-
-	// 상품목록에 출력될 상품 개수 세기
-	public int selectCountProducts(List<String> collection) {
-		return dao.selectCountProducts(collection);
 	}
 	
-	/* 상품검색 */
-	// 상품 검색해서 목록 불러오기
-	public List<Product1VO> searchProduct(String[] arg0, String arg1, String arg2, int arg3) {
-		return dao.searchProduct(arg0, arg1, arg2, arg3);
+	//관리자페이지에서 상품목록 불러오기
+	public List<Product1VO> selectProducts(String param1){
+		return dao.selectProducts(param1);
+	}
+	public List<Product1VO> selectProductByCate1(String param1,String arg1){
+		return dao.selectProductByCate1(param1,arg1);
 	}
 	
-	//검색한 상품 개수 세기
-	public int selectCountProductByKeyword(String param2, String arg2){
-		return dao.selectCountProductByKeyword(param2, arg2);
+	public List<Product1VO> selectProductByCheckBox(List<String> checkBoxArr){
+		return dao.selectProductByCheckBox(checkBoxArr);
 	}
-
-	/* 검색결과 창 페이징 처리 */
-	// 현재 페이지 번호
-	public int getCurrentPage(String pg) {
-		int currentPage = 1;
-
-		if (pg != null) {
-			currentPage = Integer.parseInt(pg);
-		}
-
-		return currentPage;
-	}
-
-	// 페이지 시작값
-	public int getLimitStart(int currentPage) {
-		return (currentPage - 1) * 10;
-	}
-
-	// 마지막 페이지 번호
-	public int getLastPageNum(int total) {
-		int lastPageNum = 0;
-
-		if (total % 10 == 0) {
-			lastPageNum = total / 10;
-		} else {
-			lastPageNum = total / 10 + 1;
-		}
-
-		return lastPageNum;
-	}
-
-	// 페이지 시작번호
-	public int getPageStartNum(int total, int arg1) {
-		return total - arg1;
-	}
-
-	// 페이지 그룹
-	public int[] getPageGroup(int currentPage, int lastPageNum) {
-		int groupCurrent = (int) Math.ceil(currentPage / 10.0);
-		int groupStart = (groupCurrent - 1) * 10 + 1;
-		int groupEnd = groupCurrent * 10;
-
-		if (groupEnd > lastPageNum) {
-			groupEnd = lastPageNum;
-		}
-
-		int[] groups = { groupStart, groupEnd };
-
-		return groups;
-	}
-
-	// 파일 업로드
+	
+	//파일 업로드
 	@Value("${spring.servlet.multipart.location}")
 	private String uploadPath;
-
+	
 	public void fileUpload(Product1VO vo) {
 		MultipartFile file1 = vo.getFile1();
 		MultipartFile file2 = vo.getFile2();
@@ -140,19 +61,19 @@ public class AdminService {
 		MultipartFile file7 = vo.getFile7();
 		MultipartFile file8 = vo.getFile8();
 		MultipartFile file9 = vo.getFile9();
-
-		if (!file1.isEmpty()) {
-			// 시스템 경로
+		
+		if(!file1.isEmpty()) {
+			//시스템 경로
 			String path = new File(uploadPath).getAbsolutePath();
-
-			// 새 파일명 생성
+			
+			//새 파일명 생성
 			String oriName = file1.getOriginalFilename();
 			String ext = oriName.substring(oriName.lastIndexOf("."));
-			String newName = UUID.randomUUID().toString() + ext;
-
+			String newName = UUID.randomUUID().toString()+ext;
+			
 			vo.setThumb1(newName);
-
-			// 파일 저장
+			
+			//파일 저장
 			try {
 				file1.transferTo(new File(path, newName));
 			} catch (IllegalStateException e) {
@@ -323,5 +244,5 @@ public class AdminService {
 		}
 
 	}
-
+	
 }
